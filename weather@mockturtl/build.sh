6bf4422ee38d6c43313de626fbb68ade515e0e98
Kirill Lappo <kirill.lappo@oxagile.com>
2021-04-11 17:08:01 +0300

    build.sh : simplified DIR calculation; quoted pathes fix space usage in pathes

--- weather@mockturtl/build.sh
+++ weather@mockturtl/build.sh
@@ -3,18 +3,13 @@
 # - typescript installed
 
 # Getting bash script file location
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
+scriptName=$0
+DIR=$(dirname "$(realpath ${scriptName})")
 
 # Save current dir for convenience
-path=${PWD}
+originalDir="${PWD}"
 
-cd $DIR/src/3_8
+cd "$DIR/src/3_8"
 echo Building 3.8...
 tsc -p tsconfig.json
-cd $DIR
+cd "$originalDir"
